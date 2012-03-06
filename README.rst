578f4a90b108a96e6f6ccab2c3c9108f1eec6eff
Dominique Guardiola <contact@quinode.fr>
2012-03-07 00:28:11 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,10 @@
+coop-geo, map tools for django-coop
+===============================================
+
+GeoDjango fields and geo widgets for django-coop
+
+License
+=======
+
+coop-geo uses the same license as Django (BSD).
+coop-geo development was funded by `CREDIS <http://credis.org/>`_, FSE (European Social Fund) and Conseil Regional d'Auvergne.
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('coop_geo').__version__
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name ='coop-geo',
+    version = VERSION,
+    description ='GeoDjango fields and geo widgets for django-coop',
+    long_description = open('README.rst').read(),
+    packages = ['coop_geo',
+                'coop_geo.management',
+                'coop_geo.migrations',],
+    include_package_data = True,
+    author = 'Etienne Loks',
+    author_email = 'etienne.loks@peacefrogs.net',
+    license ='BSD',
+    url = "https://github.com/quinode/coop-geo/",
+    download_url = "https://github.com/quinode/coop-geo/tarball/master",
+    zip_safe = False,
+    install_requires = ['coop',
+                        'django-floppyforms==0.4.7',
+                        ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+)
+
