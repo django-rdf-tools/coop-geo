4a5e6abb77bd2474e0e3d84d1260fcb8dbc31751
Étienne Loks <etienne.loks@peacefrogs.net>
2012-11-21 01:52:57 +0100

    Import shapefile script: add a --list option to list shapefile columns - minor code improvments

--- coop_geo/management/commands/import_shapefile.py
+++ coop_geo/management/commands/import_shapefile.py
@@ -1,36 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+Command to import areas from a shapefile.
+'''
+
 import os
 from optparse import make_option
+import sys
 import tempfile
 import zipfile
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.gis.gdal import DataSource
-from django.contrib.gis.geos import MultiPolygon, Polygon
+from django.contrib.gis.geos import MultiPolygon
 
 from coop_geo import models
 
 class Command(BaseCommand):
+    '''
+    Command to import areas from a shapefile.
+    '''
     args = '<zipped_shapefile> <areatype_idx> [<name_col> <reference_col>]'
-    help = 'Import area from a shapefile'
+    help = 'Import areas from a shapefile.\n\n"areatype_idx" is the textual '\
+           'index of the area type. If this area type doesn\'t exist it is '\
+           'created.\n"name_col" and "reference_col" are the columns to use '\
+           'in the shapefile data respectively for the name and the '\
+           'reference.\n\nCreation and update are managed. '\
+           'Update is managed according to the reference_col and the area type.'
     option_list = BaseCommand.option_list + (
         make_option('-q', '--quiet',
             action="store_true",
             dest="quiet", default=False,
             help="no output"),
+        make_option('-l', '--list',
+            action="store_true",
+            dest="list", default=False,
+            help="list available columns in the shapefile"),
         )
 
     def handle(self, *args, **options):
-        if not args or len(args) < 2:
-            raise CommandError(u"Args missing")
         quiet = 'quiet' in options and options['quiet']
+        list_columns = 'list' in options and options['list']
+        if not args or len(args) < 1 or (not list_columns and len(args) < 2):
+            raise CommandError(u"Args missing")
         zipped_shapefile = args[0]
-        areatype_idx = args[1]
+        areatype_idx = args[1] if len(args) > 1 else ''
         name_col = args[2] if len(args) > 2 else ''
         reference_col = args[3] if len(args) > 3 else ''
 
-        areatype, created = models.AreaType.objects.get_or_create(
+        areatype = None
+        if areatype_idx:
+            areatype, created = models.AreaType.objects.get_or_create(
                                     txt_idx=areatype_idx,
                                     defaults={
                                         'label': areatype_idx.replace('-', ' '
@@ -44,19 +64,15 @@ class Command(BaseCommand):
         except zipfile.BadZipfile:
             raise CommandError(u"Bad zip file")
         tmpdir = tempfile.mkdtemp()
-        prjfilename, dbffilename = None, None
-        shpfilename, shxfilename = None, None
+        prjfilename = None
+        shpfilename = None
         for filename in flz.namelist():
             flz.extract(filename, tmpdir)
             full_name = os.sep.join([tmpdir, filename])
             if filename.endswith('.prj'):
                 prjfilename = full_name
-            elif filename.endswith('.dbf'):
-                dbffilename = full_name
             elif filename.endswith('.shp'):
                 shpfilename = full_name
-            elif filename.endswith('.shx'):
-                shxfilename = full_name
         srid = None
         if prjfilename:
             try:
@@ -69,12 +85,16 @@ class Command(BaseCommand):
                     srid = srs.GetAuthorityCode(None)
             except ImportError:
                 srid = 4326
-                if not quiet:
-                    self.stdout.write('WARN: module gdal is not installed. SRID '
-                        'cannot be read from proj\n      file. Default to '
+                if not quiet and not list_columns:
+                    self.stdout.write('WARN: module gdal is not installed. SRID'
+                        ' cannot be read from proj\n      file. Default to '
                         'WGS84 projection (latitude/longitude).\n')
-        ds = DataSource(shpfilename)
-        lyr = ds[0]
+        datasource = DataSource(shpfilename)
+        lyr = datasource[0]
+        if list_columns:
+            self.stdout.write('Available columns in this shapefile are: %s\n' %
+                              ", ".join(lyr.fields))
+            sys.exit(0)
         name_idx, reference_idx = None, None
         if name_col or reference_col:
             for idx, name in enumerate(lyr.fields):
@@ -87,7 +107,7 @@ class Command(BaseCommand):
                      u'column are: %s.' % (name_col, ", ".join(lyr.fields)))
         if reference_col and reference_idx == None:
             raise CommandError(u'"%s" is not a valid column name. Available '
-                     u'column are: %s.' % (reference_col, ", ".join(lyr.fields)))
+                    u'column are: %s.' % (reference_col, ", ".join(lyr.fields)))
         if reference_idx == None:
             reference_col = lyr.fields[0]
         if name_idx == None:
